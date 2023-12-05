f3abea1ca7fe31b720edf1c3cb5b14b81e669c0f
Reilly Shofner <reilly.shofner@gmail.com>
2023-12-04 21:23:12 -0800

    complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+ // for (let i = 0; i < gifts.length; i++) {
+   // console.log(`Wrapped ${gifts[i]} and added a bow!`);
+ // }
+
+  //return gifts;
+//}
+
+//wrapGifts(gifts);
+debugger;
+function writeCards(name, eventName) {
+    let msgArray = [];
+    for (let i = 0; i < name.length; i++){
+        msgArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return msgArray;
+} 
+function countDown() {
+let countDown = [0];
+while (countDown < 11) {
+    console.log(countDown++)
+} 
+}
